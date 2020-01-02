3746abfbb4eee64bdd286ce6d9b262cf2574158b
Elly (박수현) <elly@treenod.com>
2020-01-02 18:30:19 +0900

    연습문제 추가, todo 프로젝트 생성

---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <input type="text" v-model="newTodoItem">
+        <button v-on:click="addTodo">추가</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            newTodoItem: ''
+        }
+    },
+    methods: {
+        addTodo() {
+            if (this.newTodoItem) {
+                let value = this.newTodoItem && this.newTodoItem.trim()
+                localStorage.setItem(value, value)
+                this.clearInput()
+            }            
+        },
+        clearInput() {
+            this.newTodoItem = ''
+        }
+    }
+}
+</script>
+
+<style>
+</style>
