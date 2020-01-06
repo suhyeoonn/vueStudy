36f6b41b5ee48f59fd19541430a5bfe5ca59c381
elly <suhyeon1740@gmail.com>
2020-01-06 20:01:40 +0900

    vue cli components 문제 추가

---
+++ vue-cli-quiz/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Links/>
+    <Plugins/>
+    <Ecosystem/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Links from './components/Links.vue'
+import Plugins from './components/Plugins.vue'
+import Ecosystem from './components/Ecosystem'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    Links,
+    Plugins,
+    Ecosystem
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-cli-quiz/src/components/Plugins.vue
@@ -0,0 +1,9 @@
+<template>
+    <section>
+        <h3>Installed CLI Plugins</h3>
+        <ul>
+        <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
+        <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+        </ul>
+    </section>
+</template>
