68b52434cb51b3c0a70a0fc1fd23eb466f114d4a
Elly (박수현) <elly@treenod.com>
2020-01-06 18:32:51 +0900

    vue 웹사이트 연습

---
+++ web-site/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <Menu></Menu>        
+    <Main>
+      <router-view></router-view>
+    </Main>
+  </div>
+</template>
+
+<script>
+import Menu from './components/Menu.vue'
+import Main from './components/Main.vue'
+
+var routes = [
+    { path: '/main', component: Main }    
+]
+
+var router = new VueRouter({        
+    routes
+})
+
+export default {
+  name: 'app',
+  components: {
+    Menu, Main
+  },
+  router
+}
+
+</script>
+
+<style>
+* {
+  box-sizing: inherit;
+}
+body {
+  overflow-y: scroll;
+    -ms-overflow-style: -ms-autohiding-scrollbar;
+    margin: 0;
+    line-height: 1.57;
+    word-wrap: break-word;
+    word-break: break-all;
+    text-align: justify;    
+    font-size: 14px;
+    color: #001b34;
+}
+#app {
+
+}
+</style>
---
+++ web-site/src/components/Menu.vue
@@ -0,0 +1,81 @@
+<template>
+    <div id="menu">
+        <header>
+            <h1><a href="#">OPENKNOWL</a></h1>
+            <nav>
+                <ul>
+                    <router-link to="/main">Vision</router-link>
+                    <router-link to="/login">Services</router-link>
+                    <!-- <li>
+                        <a href="/./vision" class="" title="오픈놀 비전">Vision</a>
+                    </li>
+                    <li>
+                        <a href="/./services" class="router-link-exact-active router-link-active" title="오픈놀 서비스">Services</a>
+                    </li> -->
+                    <li>
+                        <a href="/./history" class="" title="오픈놀 연혁">History</a>
+                    </li>
+                    <li>
+                        <a href="/./recruit" class="" title="오픈놀 채용">Recruit</a>
+                    </li>
+                    <li>
+                        <a href="/./contact" class="" title="오픈놀 문의">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <footer>
+            <a href="#">blog</a>
+            <a href="#">facebook</a>
+        </footer>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Menu'  
+}
+</script>
+
+<style scoped>
+    #menu {
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 999;
+        width: 256px;
+        padding-top: 48px;
+        padding-left: 48px;
+        padding-right: 48px;
+        border-right: 1px solid #bcc5d0;
+        background-color: hsla(0,0%,100%,.95);
+    }    
+    nav {
+        margin-top: 49px;
+        letter-spacing: .25em;
+        text-transform: uppercase;
+    }
+    ul {
+        margin-top: 0;
+        margin-bottom: 0;
+        padding-left: 0;
+        list-style-type: none;
+    }
+    li {
+        margin-top: 32px;
+    }
+    a {
+        color: inherit;
+        text-decoration: initial;
+    }
+    footer {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        padding-bottom: 48px;
+        padding-left: 48px;
+        padding-right: 48px;
+    }
+</style>
